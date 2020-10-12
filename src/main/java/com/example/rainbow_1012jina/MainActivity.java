9b0fd2d6e1b08e572c07cc2e4649ff88bc5218e9
jina <2017101614@khu.ac.kr>
2020-10-12 21:21:25 +0900

    1012jina

--- src/main/java/com/example/rainbow_1012jina/MainActivity.java
+++ src/main/java/com/example/rainbow_1012jina/MainActivity.java
@@ -1,24 +1,32 @@
 package com.example.rainbow_1012jina;
 
+import android.content.Intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 
 public class MainActivity extends AppCompatActivity {
+    Button settingbutton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button settingbutton = (Button) findViewById(R.id.go_setting);
+        settingbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), setting_time.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
-    public MaterialCalendarView materialCalendarView;
-
-
-
-
 }
+
+
+
+
--- src/main/java/com/example/rainbow_1012jina/setting_time.java
+++ src/main/java/com/example/rainbow_1012jina/setting_time.java
@@ -1,10 +1,51 @@
 package com.example.rainbow_1012jina;
 import android.app.Activity;
 import android.os.Bundle;
-public class setting_time extends Activity{
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class setting_time extends AppCompatActivity {
+
+    public int hNumber;
+    public int mNumber;
+
+    public TextView hTextNumber;
+    public TextView mTextNumber;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_time_page);
+        if(savedInstanceState!=null){
+            hNumber = savedInstanceState.getInt("hour",0);
+            mNumber = savedInstanceState.getInt("min",0);
+        }
+        hTextNumber = (TextView) findViewById(R.id.hourNumber);
+        hTextNumber.setText(hNumber+"");
+
+        mTextNumber = (TextView) findViewById(R.id.minNumber);
+        mTextNumber.setText(mNumber+"");
+    }
+
+    public void mOnClick(View v){
+        switch (v.getId()){
+            case R.id.hourInc:
+                hNumber++;
+                break;
+            case R.id.hourDec:
+                hNumber--;
+                break;
+            case R.id.minInc:
+                mNumber+=15;
+                break;
+            case R.id.minDec:
+                mNumber-=15;
+                break;
+        }
     }
 }

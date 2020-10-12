30a6975fe2cb61f14baf2aa664785aa054ce732d
jina <2017101614@khu.ac.kr>
2020-10-12 15:14:00 +0900

    1012jina

---
+++ src/main/java/com/example/rainbow_1012jina/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.rainbow_1012jina;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public MaterialCalendarView materialCalendarView;
+
+
+
+
+}
---
+++ src/main/java/com/example/rainbow_1012jina/MainPage.java
@@ -0,0 +1,16 @@
+package com.example.rainbow_1012jina;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MainPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+

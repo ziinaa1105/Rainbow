a0cc8c435e1537baf55178e0029db4ea8a4485b8
jina <2017101614@khu.ac.kr>
2020-10-12 17:05:40 +0900

    1012jina

---
+++ src/main/java/com/example/rainbow_1012jina/date_study.java
@@ -0,0 +1,10 @@
+package com.example.rainbow_1012jina;
+import android.app.Activity;
+import android.os.Bundle;
+public class date_study extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.date_study_page);
+    }
+}
---
+++ src/main/java/com/example/rainbow_1012jina/setting_time.java
@@ -0,0 +1,10 @@
+package com.example.rainbow_1012jina;
+import android.app.Activity;
+import android.os.Bundle;
+public class setting_time extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_time_page);
+    }
+}
